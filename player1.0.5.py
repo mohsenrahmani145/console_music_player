78ce2a0747f76e5aced751f2141002bf5d6a28c3
mohsenrahmani145 <104733355+mohsenrahmani145@users.noreply.github.com>
2022-07-06 11:19:24 +0430

    Update player1.0.5.py

--- player1.0.5.py
+++ player1.0.5.py
@@ -16,7 +16,6 @@ def player(filename):
         time.sleep(1)
 
 
-    
 def timer(length):
     zerotime = 0
     length = length
@@ -31,52 +30,51 @@ def timer(length):
             print("___________________________")
             sys.exit()
 
+
 def main():
- 
+    repeat = ""
     while True:
         try:
-            
+
             print("Please enter a file name: ")
             try:
                 input_name = str(input())
             except KeyboardInterrupt:
                 sys.exit()
-            
 
             if input_name == "exit":
                 sys.exit()
-            if input_name != "repeat" and input_name.split('.')[-1].lower() == "mp3":
-                repeat = input_name
             elif input_name == "repeat":
                 input_name = repeat
-            length = int(MP3(f"{input_name}").info.length) 
+            length = int(MP3(f"{input_name}").info.length)
             info = int(MP3(input_name).info.bitrate)
             p1 = threading.Thread(target=timer, args=(length,))
-            
+
             print("Name: " + input_name.split("\\")[-1])
             try:
-                print("Artist: " + ID3(input_name)['TPE1'].text[0]) #Artist
-                print("Track: " + ID3(input_name)['TIT2'].text[0]) #Track   
-            
+                print("Artist: " + ID3(input_name)['TPE1'].text[0])  # Artist
+                print("Track: " + ID3(input_name)['TIT2'].text[0])  # Track
+
             except Exception:
                 pass
 
             print(f"Bitrate: {int(info/1000)} kbps")
-            print("Length: " +
-                str(datetime.timedelta(seconds=length))) #length
+            print("Length: " + str(datetime.timedelta(seconds=length)))  # length
             p1.daemon = True
             p1.start()
-            
+
             player(input_name)
+            if input_name != "repeat" and input_name.split('.')[-1].lower() == "mp3":
+                repeat = input_name
 
         except Exception:
             print("File not found")
         except KeyboardInterrupt:
             mixer.music.stop()
             sys.exit()
-                        
 
-        
+
+
 os.system('cls||clear')
 print("Wellcome to console music player v1.0.5")
 main()
