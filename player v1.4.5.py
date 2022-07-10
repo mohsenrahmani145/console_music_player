22da479e8ff7f3feb7349ad2d1ed4aa97deee0e6
mohsenrahmani145 <104733355+mohsenrahmani145@users.noreply.github.com>
2022-07-11 00:35:03 +0430

    Update player v1.4.5.py

--- player v1.4.5.py
+++ player v1.4.5.py
@@ -3,6 +3,7 @@ import sys
 import threading
 import time
 import os
+import keyboard
 from mutagen.mp3 import MP3
 from mutagen.id3 import ID3
 from pygame import mixer
@@ -10,11 +11,23 @@ from tkinter import Tk
 from tkinter import filedialog
 
 
+def key_event(hot_key):
+    try:
+        keyboard.record(hot_key)
+        key_stop.set()
+    except Exception:
+        pass
+
+
 def player(filename):
+    key_stop.clear()
     mixer.init()
     mixer.music.load(f"{filename}")
     mixer.music.play()
     while mixer.music.get_busy():
+        if key_stop.is_set():
+            mixer.music.stop()
+            print('Audio stopped playing')
         time.sleep(1)
 
 
@@ -31,7 +44,6 @@ def timer(length):
         if stop_thread.is_set():
             break
         if zerotime > length:
-            print("___________________________")
             sys.exit()
 
 
@@ -43,16 +55,17 @@ def main():
         try:
             print("Please enter a file name: ")
             input_name = str(input())
-
+            if input_name.find("\"") == 0:
+                input_name = input_name.split("\"")[1]
             if input_name.lower() == "exit":
                 sys.exit()
-            elif input_name.lower() == "repeat":
+            if input_name.lower() == "repeat":
                 input_name = repeat
-            elif input_name.lower() == 'import':
+            if input_name.lower() == 'import':
                 root = Tk()
                 root.withdraw()
-                file_path = filedialog.askopenfilename(
-                    filetypes=[("MP3 - MPEG Layer-3 Audio", ".mp3")])
+                file_path = filedialog.askopenfilename(filetypes=[(
+                    "MP3 - MPEG Layer-3 Audio", "*.mp3")], title="Select A File", parent=root)
                 input_name = file_path.replace('/', '\\')
 
             length = int(MP3(f"{input_name}").info.length)
@@ -65,12 +78,18 @@ def main():
                 pass
             print(f"Bitrate: {int(info/1000)} kbps")
             print("Length: " + str(datetime.timedelta(seconds=length)))  # length
+            stop_thread.clear()
+            time_elapsed = threading.Thread(target=timer, args=(length,))
+            time_elapsed.start()
+            stop_event = threading.Thread(target=key_event,args=(("ctrl","s"),))
+            stop_event.daemon = True
+            stop_event.start()
 
-            p1 = threading.Thread(target=timer, args=(length,))
-            p1.start()
             player(input_name)
             if input_name != "repeat" and input_name.split('.')[-1].lower() == "mp3":
                 repeat = input_name
+            print("___________________________")
+            stop_thread.set()
 
         except Exception:
             print("File not found")
@@ -84,8 +103,8 @@ def main():
             sys.exit()
 
 
-
 stop_thread = threading.Event()
+key_stop = threading.Event()
 os.system('cls||clear')
 print("Wellcome to console music player v1.4.5")
 main()
